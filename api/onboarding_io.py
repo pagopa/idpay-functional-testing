d35fad0cb0220a61b6169839a21913b4100ef810
TommasoLencioni <tommaso.lencioni@pagopa.it>
2023-03-10 18:25:56 +0100

    [IDP-462] add onboarding endpoints

--- api/onboarding_io.py
+++ api/onboarding_io.py
@@ -1,15 +1,76 @@
+"""Module containing onboarding endpoint
+"""
 import requests
 
 
 def accept_terms_and_condition(url, token, initiative_id):
-    """API to obtain an IO like token from a stub
-        :param url: loginUrl
+    """API to accept terms and condition
+        :param url: terms and condition url
         :param token: token IO
         :param initiative_id: initiative on which onboard the user
         :returns:  The response of the call.
         :rtype: requests.Response
     """
     return requests.put(url,
+                        headers={
+                            'Content-Type': 'application/json',
+                            "Authorization": f'Bearer {token}',
+                        },
+                        json={'initiativeId': initiative_id},
+                        timeout=5000)
+
+
+def check_prerequisites(url, token, initiative_id):
+    """API to put check prerequisites
+        :param url: check prerequisites url
+        :param token: token IO
+        :param initiative_id: initiative on which onboard the user
+        :returns:  The response of the call.
+        :rtype: requests.Response
+    """
+    return requests.put(url,
+                        headers={
+                            'Content-Type': 'application/json',
+                            "Authorization": f'Bearer {token}',
+                        },
+                        json={'initiativeId': initiative_id},
+                        timeout=5000)
+
+
+def pdnd_autocertification(url, token, initiative_id):
+    """API to put autocertification
+        :param url: autocertification url
+        :param token: token IO
+        :param initiative_id: initiative on which onboard the user
+        :returns:  The response of the call.
+        :rtype: requests.Response
+    """
+    return requests.put(url,
+                        headers={
+                            'Content-Type': 'application/json',
+                            "Authorization": f'Bearer {token}',
+                        },
+                        json={'initiativeId': initiative_id,
+                              "pdndAccept": 'true',
+                              "selfDeclarationList": [
+                                  {
+                                      "_type": "boolean",
+                                      "code": "1",
+                                      "accepted": 'true'
+                                  }]
+                              },
+                        timeout=5000)
+
+
+def status_onboarding(url, token, initiative_id):
+    """API to get onboarding status
+        :param url: check url
+        :param token: token IO
+        :param initiative_id: initiative on which onboard the user
+        :returns:  The response of the call.
+        :rtype: requests.Response
+    """
+    return requests.get(url,
                         headers={
                             'Content-Type': 'application/json',
                             "Authorization": f'Bearer {token}',
