d753e832fcae7b143a6f0cb45fca59b6e520ebc8
TommasoLencioni <tommaso.lencioni@pagopa.it>
2023-03-15 18:18:57 +0100

    [IDP-451] add IDPAY APIs

---
+++ api/idpay.py
@@ -0,0 +1,17 @@
+import requests
+
+from conf.configuration import settings
+
+
+def timeline(initiative_id, token):
+    """API to get timeline of a user
+        :param token: token IO
+        :returns:  The response of the call.
+        :rtype: requests.Response
+    """
+    return requests.get(
+        f'{settings.base_path.IO}{settings.IDPAY.domain}{settings.IDPAY.endpoints.timeline}/{initiative_id}/?page=0&size=10',
+        headers={
+            "Authorization": f'Bearer {token}',
+        },
+        timeout=5000)
