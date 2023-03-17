cde4c00804c32f35e3547799d2a47a31365cdef4
TommasoLencioni <tommaso.lencioni@pagopa.it>
2023-03-17 17:13:09 +0100

    [IDP-451] refactor IO onboarding test

--- tests/test_cashback_like/test_onboarding_io.py
+++ tests/test_cashback_like/test_onboarding_io.py
@@ -2,10 +2,8 @@
 """
 import pytest
 
-from api.onboarding_io import accept_terms_and_condition, check_prerequisites, pdnd_autocertification, status_onboarding
-from api.token_io import introspect, login
-from conf.configuration import settings, secrets
-from util import dataset_utility
+from util.dataset_utility import fake_fc
+from util.utility import onboard_io
 
 
 @pytest.mark.IO
@@ -15,24 +13,6 @@ def test_onboard_io():
     """Onboarding process through IO
     """
 
-    test_fc = dataset_utility.fake_fc()
+    test_fc = fake_fc()
 
-    res = login(test_fc)
-    token = res.content.decode('utf-8')
-    res = introspect(token)
-    assert res.json()['fiscal_code'] == test_fc
-    res = accept_terms_and_condition(token, secrets.initiatives.cashback_like.id)
-    assert res.status_code == 204
-    res = check_prerequisites(
-        token,
-        secrets.initiatives.cashback_like.id)
-    assert res.status_code == 200
-    res = pdnd_autocertification(
-        token,
-        secrets.initiatives.cashback_like.id)
-    assert res.status_code == 202
-    res = status_onboarding(
-        token,
-        secrets.initiatives.cashback_like.id)
-    assert res.status_code == 200
-    assert res.json()['status'] in ('ONBOARDING_OK', 'ON_EVALUATION')
+    assert onboard_io(test_fc).json()['status'] == 'ONBOARDING_OK'
