9a8b9cb9c611e2e31cce61df751f96ecf69b3cdf
TommasoLencioni <tommaso.lencioni@pagopa.it>
2023-03-23 15:42:11 +0100

    [IDP-451] add get wallet API

--- api/idpay.py
+++ api/idpay.py
@@ -18,6 +18,22 @@ def timeline(initiative_id, token):
         timeout=5000)
 
 
+def wallet(initiative_id, token):
+    """API to get citizen wallet.
+        :param initiative_id: ID of the initiative of interest.
+        :param token: token IO.
+        :returns: the response of the call.
+        :rtype: requests.Response
+    """
+    return requests.get(
+        f'{settings.base_path.IO}{settings.IDPAY.domain}{settings.IDPAY.endpoints.wallet}/{initiative_id}',
+        headers={
+            "Authorization": f'Bearer {token}',
+            'Content-Type': 'application/json',
+        },
+        timeout=5000)
+
+
 def enroll_iban(initiative_id, token, body):
     """API to enroll an IBAN
         :param initiative_id: ID of the initiative of interest.
