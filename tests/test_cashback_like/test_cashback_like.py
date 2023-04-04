304324297bdea65cd340de556647afd233a8df2b
TommasoLencioni <tommaso.lencioni@pagopa.it>
2023-04-04 16:18:00 +0200

    [IDP-576] add use case 27

--- tests/test_cashback_like/test_cashback_like.py
+++ tests/test_cashback_like/test_cashback_like.py
@@ -983,3 +983,58 @@ def test_onboarding_after_unsubscribe():
     expect_wallet_counters(expected_amount_left, expected_accrued, token_b, initiative_id)
 
     clean_trx_files(curr_file_name)
+
+
+@pytest.mark.IO
+@pytest.mark.onboard
+@pytest.mark.enroll
+@pytest.mark.reward
+@pytest.mark.cashback
+@pytest.mark.need_fix
+@pytest.mark.use_case('1.27')
+def test_homocode_onboarding():
+    test_fc = fake_fc()
+    curr_iban = dataset_utility.fake_iban('00000')
+    pan = fake_pan()
+    token = get_io_token(test_fc)
+
+    # 1.27.1
+    onboard_io(test_fc, initiative_id).json()
+    retry_wallet(expected=wallet_statuses.not_refundable, request=wallet, token=token,
+                 initiative_id=initiative_id, field='status', tries=3, delay=3,
+                 message='Not subscribed')
+    # 1.27.2
+    card_enroll(test_fc, pan, initiative_id)
+    retry_wallet(expected=wallet_statuses.not_refundable_only_instrument, request=wallet, token=token,
+                 initiative_id=initiative_id, field='status', tries=3, delay=3,
+                 message='Card not enrolled')
+
+    # 1.27.3
+    iban_enroll(test_fc, curr_iban, initiative_id)
+    retry_wallet(expected=wallet_statuses.refundable, request=wallet, token=token,
+                 initiative_id=initiative_id, field='status', tries=3, delay=3,
+                 message='IBAN not enrolled')
+
+    amount = floor(random.random() * max_amount)
+    transaction = custom_transaction(pan, amount)
+    trx_file_content = '\n'.join([transactions_hash(transaction), transaction])
+    res, curr_file_name = encrypt_and_upload(trx_file_content)
+    # 1.27.4
+    assert res.status_code == 201
+    # 1.27.4
+    retry_timeline(expected=timeline_operations.transaction, request=timeline, num_required=1, token=token,
+                   initiative_id=initiative_id, field='operationType', tries=10, delay=3,
+                   message='Transaction not received')
+
+    expected_accrued = round(floor(amount * cashback_percentage) / 10000, 2)
+    expected_amount_left = round(float(budget_per_citizen - expected_accrued), 2)
+
+    # 1.27.5
+    expect_wallet_counters(expected_amount_left, expected_accrued, token, initiative_id)
+
+    clean_trx_files(curr_file_name)
+
+    token1 = get_io_token(test_fc)
+    res = accept_terms_and_condition(token1, initiative_id)
+    # 1.27.7
+    assert res.status_code != 204
