5b308671e0be20c4b5b55a7fd9ee170070702207
TommasoLencioni <tommaso.lencioni@pagopa.it>
2023-06-20 10:14:19 +0200

    [IDP-1056] fix FC precision

--- bdd/steps/dataset_steps.py
+++ bdd/steps/dataset_steps.py
@@ -25,7 +25,7 @@ def step_citizen_fc_exact_or_random(context, citizen_fc):
 def step_citizen_fc_from_name_age_and_precision(context, citizen_name: str, age: int, precision: str):
     citizen_fc = fake_fc(age=age)
     if precision == 'at most':
-        current_date = datetime.datetime.now() + datetime.timedelta(days=random.randint(30, 90))
+        current_date = datetime.datetime.now() - datetime.timedelta(days=random.randint(30, 90))
         current_month = current_date.month
         current_day = current_date.day
         citizen_fc = fake_fc(age=age, custom_month=current_month, custom_day=current_day)
