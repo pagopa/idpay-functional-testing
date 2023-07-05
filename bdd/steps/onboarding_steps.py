830c4cf78f4487e9da55751cf7552607ded7d03b
TommasoLencioni <tommaso.lencioni@pagopa.it>
2023-07-05 12:45:48 +0200

    [IDP-1256] fix message

--- bdd/steps/onboarding_steps.py
+++ bdd/steps/onboarding_steps.py
@@ -78,7 +78,7 @@ def step_check_onboarding_status(context, citizen_name, status):
     else:
         retry_wallet(expected=wallet_statuses.refundable, request=wallet, token=token_io,
                      initiative_id=context.initiative_id, field='status', tries=3, delay=3,
-                     message='Card not enrolled')
+                     message='Not refundable')
         retry_timeline(expected=timeline_operations.onboarding, request=timeline, num_required=1, token=token_io,
                        initiative_id=context.initiative_id, field='operationType', tries=10, delay=3,
                        message='Not onboard')
