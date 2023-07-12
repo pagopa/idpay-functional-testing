75075d33e867d500e7090d2b24efce64dc5f319e
TommasoLencioni <tommaso.lencioni@pagopa.it>
2023-07-12 12:24:04 +0200

    [IDP-1256] rename method

--- bdd/steps/discount_transaction_steps.py
+++ bdd/steps/discount_transaction_steps.py
@@ -36,7 +36,8 @@ def step_when_merchant_tries_to_create_a_transaction(context, merchant_name, trx
 
 @when(
     'the merchant {merchant_name} tries to generate the transaction {trx_name} of amount {amount_cents} cents with wrong acquirer ID')
-def step_when_merchant_tries_to_create_a_transaction(context, merchant_name, trx_name, amount_cents):
+def step_when_merchant_tries_to_create_a_transaction_with_wrong_acquirer_id(context, merchant_name, trx_name,
+                                                                            amount_cents):
     curr_merchant_id = context.merchants[merchant_name]['id']
     context.latest_merchant_id = curr_merchant_id
 
