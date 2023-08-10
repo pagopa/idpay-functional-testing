fa4f8c266d7c4625d2f86ac8f7c26432c18f000f
TommasoLencioni <tommaso.lencioni@pagopa.it>
2023-08-10 12:08:44 +0200

    [IDP-1205] move and refactor scenario

--- tests/test_cashback_like/test_onboarding_io.py
+++ tests/test_cashback_like/test_onboarding_io.py
@@ -7,9 +7,11 @@ import string
 
 import pytest
 
+from api.idpay import get_initiative_statistics
 from api.idpay import timeline
 from api.idpay import wallet
 from api.onboarding_io import accept_terms_and_condition
+from api.onboarding_io import pdnd_autocertification
 from api.token_io import introspect
 from api.token_io import login
 from conf.configuration import secrets
@@ -17,6 +19,7 @@ from conf.configuration import settings
 from util.dataset_utility import fake_fc
 from util.dataset_utility import fake_temporary_fc
 from util.dataset_utility import get_random_unicode
+from util.utility import check_statistics
 from util.utility import get_io_token
 from util.utility import onboard_io
 
@@ -133,3 +136,34 @@ def test_timeline_without_ever_onboard():
     assert res.status_code == 404
     res = timeline(initiative_id=initiative_id, token=token_io)
     assert res.status_code == 404
+
+
+@pytest.mark.IO
+@pytest.mark.onboard
+def test_onboard_two_times():
+    """Repeat the onboarding of the same citizen and check if the counters change
+    """
+    organization_id = secrets.organization_id
+    old_statistics = get_initiative_statistics(organization_id=organization_id, initiative_id=initiative_id).json()
+
+    test_fc = fake_temporary_fc()
+
+    onboard_io(test_fc, initiative_id).json()
+    check_statistics(organization_id=organization_id, initiative_id=initiative_id, old_statistics=old_statistics,
+                     onboarded_citizen_count_increment=1, accrued_rewards_increment=0,
+                     rewarded_trxs_increment=0, skip_trx_check=True)
+    old_statistics = get_initiative_statistics(organization_id=organization_id, initiative_id=initiative_id).json()
+
+    token = get_io_token(test_fc)
+    res = accept_terms_and_condition(token, initiative_id)
+    assert res.status_code == 204
+    check_statistics(organization_id=organization_id, initiative_id=initiative_id, old_statistics=old_statistics,
+                     onboarded_citizen_count_increment=0, accrued_rewards_increment=0,
+                     rewarded_trxs_increment=0, skip_trx_check=True)
+    old_statistics = get_initiative_statistics(organization_id=organization_id, initiative_id=initiative_id).json()
+
+    res = pdnd_autocertification(token, initiative_id)
+    assert res.status_code == 202
+    check_statistics(organization_id=organization_id, initiative_id=initiative_id, old_statistics=old_statistics,
+                     onboarded_citizen_count_increment=0, accrued_rewards_increment=0,
+                     rewarded_trxs_increment=0, skip_trx_check=True)
