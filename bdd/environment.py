84d279ebbbc598313d55e1a01f00e4bfa9cbd4dc
TommasoLencioni <tommaso.lencioni@pagopa.it>
2023-08-23 17:37:04 +0200

    [IDP-1611] refactor behave hooks to create only one initiative of a kind per run and delete them all at the end of the run

--- bdd/environment.py
+++ bdd/environment.py
@@ -4,36 +4,41 @@ from conf.configuration import settings
 from util.utility import create_initiative
 
 
+def before_all(context):
+    """Initialize initiatives' IDs map
+    """
+    secrets.initiatives = {}
+
+
 def before_feature(context, feature):
+    """Create the feature's initiative if it has not been yet created for this run
+    """
     context.shared_data = {}
-    # Create the initiative given a proper first tag on feature file
+    # Create the initiative given a proper first tag on feature file (if not created in this run)
     context.curr_initiative_name = feature.tags[0]
-    secrets.initiatives[context.curr_initiative_name] = {}
-    secrets.initiatives[context.curr_initiative_name]['id'] = create_initiative(
-        initiative_name_in_settings=context.curr_initiative_name)
-    print(
-        f'Created initiative {secrets.initiatives[context.curr_initiative_name]["id"]} ({context.curr_initiative_name})')
+
+    if context.curr_initiative_name not in secrets.initiatives.keys():
+        secrets.initiatives[context.curr_initiative_name] = {}
+        secrets.initiatives[context.curr_initiative_name]['id'] = create_initiative(
+            initiative_name_in_settings=context.curr_initiative_name)
+        print(
+            f'Created initiative {secrets.initiatives[context.curr_initiative_name]["id"]} ({context.curr_initiative_name})')
 
 
-def after_scenario(context, scenario):
-    # Save the current initiative id at feature context level
-    context.shared_data['curr_initiative_id'] = context.initiative_id
+def after_all(context):
+    """Delete each initiative created during the run
+    """
 
-
-def after_feature(context, feature):
     # Prevents the deletion of the fixed initiatives
     fixed_initiatives = []
-    curr_initiative_id = context.shared_data['curr_initiative_id']
 
-    # Avoid deletion if at least one scenario failed
-    if settings.KEEP_INITIATIVES_ON_FAIL and any(scenario.status == 'failed' for scenario in feature.scenarios):
-        print('At least one scenario within this feature failed. Skipping additional actions.')
-        return
-
-    if not settings.KEEP_INITIATIVES_AFTER_TEST and curr_initiative_id not in fixed_initiatives:
-        res = delete_initiative(initiative_id=context.shared_data['curr_initiative_id'])
-        if res.status_code == 204:
-            print(f'Deleted initiative {context.shared_data["curr_initiative_id"]} ({context.curr_initiative_name})')
-        else:
-            print(
-                f'Failed to delete initiative {context.shared_data["curr_initiative_id"]} ({context.curr_initiative_name})')
+    if not settings.KEEP_INITIATIVES_AFTER_TEST:
+        for initiative_id in secrets.initiatives.values():
+            if initiative_id not in fixed_initiatives:
+                res = delete_initiative(initiative_id=initiative_id['id'])
+                if res.status_code == 204:
+                    print(
+                        f'Deleted initiative {initiative_id["id"]}')
+                else:
+                    print(
+                        f'Failed to delete initiative {initiative_id["id"]}')
