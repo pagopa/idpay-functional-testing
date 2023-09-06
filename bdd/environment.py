513586e3b6464821a7d63fe5db786a3cdb546457
TommasoLencioni <tommaso.lencioni@pagopa.it>
2023-09-06 16:37:10 +0200

    [IDP-1656] prevent fixed initiatives deletion

--- bdd/environment.py
+++ bdd/environment.py
@@ -9,7 +9,7 @@ from util.utility import create_initiative
 def before_all(context):
     """Initialize initiatives' IDs map
     """
-    secrets.initiatives = {}
+    secrets['newly_created'] = set()
 
 
 def before_feature(context, feature):
@@ -17,6 +17,8 @@ def before_feature(context, feature):
     """
     created = False
     # Create an initiative for each proper tag on feature file (if not created yet in this run)
+    if not secrets.initiatives:
+        secrets.initiatives = {}
     for curr_initiative_name in feature.tags:
         if curr_initiative_name in settings.initiatives:
             if curr_initiative_name not in secrets.initiatives.keys():
@@ -25,6 +27,7 @@ def before_feature(context, feature):
                     initiative_name_in_settings=curr_initiative_name)
                 print(
                     f'Created initiative {secrets.initiatives[curr_initiative_name]["id"]} ({curr_initiative_name})')
+                secrets['newly_created'].add(secrets.initiatives[curr_initiative_name]['id'])
                 created = True
 
     if created:
@@ -36,15 +39,15 @@ def after_all(context):
     """
 
     # Prevents the deletion of the fixed initiatives
-    fixed_initiatives = []
+    fixed_initiatives = set((x['id'] for x in secrets.initiatives.values())) - secrets.newly_created
 
     if not settings.KEEP_INITIATIVES_AFTER_TEST:
-        for initiative_id in secrets.initiatives.values():
-            if initiative_id not in fixed_initiatives:
-                res = delete_initiative(initiative_id=initiative_id['id'])
+        for initiative in secrets.initiatives.values():
+            if initiative['id'] not in fixed_initiatives:
+                res = delete_initiative(initiative_id=initiative['id'])
                 if res.status_code == 204:
                     print(
-                        f'Deleted initiative {initiative_id["id"]}')
+                        f'Deleted initiative {initiative["id"]}')
                 else:
                     print(
-                        f'Failed to delete initiative {initiative_id["id"]}')
+                        f'Failed to delete initiative {initiative["id"]}')
