1750eb1ba5b31e103d20d57073767eda735b0e86
TommasoLencioni <tommaso.lencioni@pagopa.it>
2023-09-22 17:29:24 +0200

    feat: [IDP-1792] initiative creation and deletion hooks

--- conf/conftest.py
+++ conf/conftest.py
@@ -1,21 +1,29 @@
 """Configuration function that executes before tests.
 """
+import time
+
 import pytest
 
+from api.idpay import delete_initiative
 from conf.configuration import secrets
 from conf.configuration import settings
 from util.text_formatter import TextFormat
+from util.utility import create_initiative
 
 
 def pytest_configure():
     """Configuration function that executes before tests.
     """
+    secrets['newly_created'] = set()
     if secrets == {}:
         msg = f'{TextFormat.FAIL}Missing {settings.TARGET_ENV}' \
               f' section in {settings.SECRET_PATH}{TextFormat.ENDC}\n'
         pytest.exit(msg)
     print(f'Testing environment: '
           f"{TextFormat.BOLD}{TextFormat.HEADER}{settings['TARGET_ENV'].upper()}{TextFormat.ENDC}")
+
+    create_test_initiatives('cashback_like')
+    time.sleep(settings.INITIATIVE_STARTUP_TIME_SECONDS)
 
 
 def pytest_collection_modifyitems(items):
@@ -25,3 +33,16 @@ def pytest_collection_modifyitems(items):
         for marker in item.iter_markers(name='use_case'):
             test_id = marker.args[0]
             item.user_properties.append(('use_case', test_id))
+
+
+def pytest_sessionfinish(session, exitstatus):
+    # This code will run once at the end of the pytest session
+    delete_initiative(initiative_id=secrets.initiatives['cashback_like']['id'])
+    print(f'Deleted initiative {secrets.initiatives["cashback_like"]["id"]}')
+
+
+def create_test_initiatives(initiative_name: str):
+    secrets.initiatives[initiative_name] = {}
+    secrets.initiatives[initiative_name]['id'] = create_initiative(initiative_name_in_settings=initiative_name)
+    print(f'Created initiative {secrets.initiatives[initiative_name]["id"]} ({initiative_name})')
+    secrets['newly_created'].add(secrets.initiatives[initiative_name]['id'])
