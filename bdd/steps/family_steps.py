3d4a6935bbb103f72528fbf4d6a8263d6da774cc
TommasoLencioni <tommaso.lencioni@pagopa.it>
2023-09-22 16:13:23 +0200

    fix: [IDP-1834] fix step

--- bdd/steps/family_steps.py
+++ bdd/steps/family_steps.py
@@ -4,18 +4,19 @@ from behave import given
 
 from api.mock import get_family_from_id
 from api.mock import put_mocked_family
-from api.pdv import detokenize_pdv_token
+from util.utility import detokenize_to_fc
+from util.utility import tokenize_fc
 
 
-@given('citizens <citizens> are in the same family')
+@given('{citizens} are in the same family')
 def step_given_same_family_id(context, citizens):
     citizens = json.loads(citizens)
-    citizens = (context.citizens_fc[name] for name in citizens)
-    res = put_mocked_family(family=list(citizens))
+    citizens_fc = list((context.citizens_fc[name] for name in citizens))
+    res = put_mocked_family(family=list(citizens_fc))
     assert res.status_code == 200
-    context.family_id = res.json()['familyId']
+    family_id = res.json()['familyId']
 
-    res = get_family_from_id(family_id=context.family_id)
-    assert res == 200
-    assert res.json()['familyId'] == context.family_id
-    assert set(detokenize_pdv_token(x) for x in res.json()['memberIds']) == set(citizens)
+    res = get_family_from_id(family_id=tokenize_fc(citizens[0]))
+    assert res.status_code == 200
+    assert res.json()['familyId'] == family_id
+    assert set(detokenize_to_fc(x) for x in res.json()['memberIds']) == set(citizens)
