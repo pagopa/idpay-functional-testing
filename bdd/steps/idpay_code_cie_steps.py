227253fa7eafa9acdcd589b0fd16ecfa6a57d211
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2023-09-28 16:06:53 +0200

    [IDP-1768] [IDP-1788] [IDP-1785] [IDP-1782] [IDP-1779] fix check boolean

--- bdd/steps/idpay_code_cie_steps.py
+++ bdd/steps/idpay_code_cie_steps.py
@@ -26,11 +26,11 @@ def step_check_idpay_code_status(context, status, citizen_name):
     token_io = get_io_token(context.citizens_fc[citizen_name])
     res = get_idpay_code_status(token=token_io)
     assert res.status_code == 200
-    print(res.json())
+
     if status == "ENABLED":
-        assert res.json()['isIdPayCodeEnabled'] == 'true'
+        assert res.json()['isIdPayCodeEnabled'] is True
     elif status == "NOT ENABLED":
-        assert res.json()['isIdPayCodeEnabled'] == 'false'
+        assert res.json()['isIdPayCodeEnabled'] is False
 
 
 @when('the citizen {citizen_name} enrolls the CIE')
@@ -85,7 +85,8 @@ def step_check_latest_cie_enrollment_failed(context, cause_ko):
     if cause_ko == 'THE CITIZEN IS NOT ONBOARD':
         assert context.latest_enrollment_response.status_code == 404
         assert context.latest_enrollment_response.json()['code'] == 404
-        assert context.latest_enrollment_response.json()['message'] == 'The requested initiative is not active for the current user!'
+        assert context.latest_enrollment_response.json()[
+                   'message'] == 'The requested initiative is not active for the current user!'
     elif cause_ko == 'THE CITIZEN IS UNSUBSCRIBED':
         assert context.latest_enrollment_response.status_code == 400
         assert context.latest_enrollment_response.json()['code'] == 400
@@ -156,4 +157,3 @@ def step_check_status_instrument_cie(context, status, citizen_name):
         retry_timeline(expected=timeline_operations.rejected_delete_instrument, request=timeline, token=token_io,
                        initiative_id=initiative_id, field='operationType', num_required=1, tries=50,
                        delay=1, message='Delete card not rejected')
-
