d54f3da84aab0cfcb821b95f44c75ddec6de4e85
TommasoLencioni <tommaso.lencioni@pagopa.it>
2023-10-05 16:19:29 +0200

    fix: [IDP-1289] fix citizen fiscal code initialization

--- bdd/steps/dataset_steps.py
+++ bdd/steps/dataset_steps.py
@@ -56,10 +56,10 @@ def step_citizen_fc_from_name_age_and_precision(context, citizen_name: str, age:
     context.latest_citizen_fc = citizen_fc
     context.latest_token_io = get_io_token(citizen_fc)
 
-    if context.citizens_fc is None:
-        context.citizens_fc = {}
-
-    context.citizens_fc[citizen_name] = citizen_fc
+    try:
+        context.citizens_fc[citizen_name] = citizen_fc
+    except AttributeError:
+        context.citizens_fc = {citizen_name: citizen_fc}
 
 
 @given('the citizen {citizen_name} has ISEE {isee} of type "{isee_type}"')
