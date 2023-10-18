04b013427a0798a1fb7100fa938ac7388bb7141f
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2023-10-18 09:43:42 +0200

    fix: [IDP-1943] removed useless secret

--- bdd/steps/idpay_code_steps.py
+++ bdd/steps/idpay_code_steps.py
@@ -36,7 +36,6 @@ instrument_types = settings.IDPAY.endpoints.wallet.instrument_type
 
 IV = bytes([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 KEY = secrets.idpay_code.encrypt_key_test
-PEM_KEY = secrets.idpay_code.public_encrypt_key
 
 
 @given('the citizen {citizen_name} generates the IDPay Code')
