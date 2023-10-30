2147fa943630f882813bc0dfc073e61502fa87e5
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2023-10-30 20:33:09 +0100

    fix: [IDP-2024] fix onboarding KO check

--- bdd/steps/onboarding_steps.py
+++ bdd/steps/onboarding_steps.py
@@ -172,7 +172,6 @@ def step_check_onboarding_status(context, citizen_name, status):
         assert res.status_code == 404
         res = timeline(initiative_id=context.initiative_id, token=token_io)
         assert res.status_code == 404
-        assert not res.json()['operationList']
 
     elif status == 'SUSPENDED':
         expected_status = status
