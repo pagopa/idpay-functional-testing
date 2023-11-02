788a219427ad28c24b26f4b0345c0f1984973869
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2023-11-02 17:38:52 +0100

    feat: [IDP-1943] fix step

--- bdd/features/tipo4/_transaction.feature
+++ bdd/features/tipo4/_transaction.feature
@@ -13,5 +13,5 @@ Feature: Transaction in a closed initiative
   @transaction_idpay_code
   Scenario: Transaction X to be paid by IDPay Code is not generated on a closed initiative
     Given the random merchant 1 is onboard
-    When the merchant 1 tries to generates the transaction X of amount 10000 cents to be paid by IDPay Code through MIL
+    When the merchant 1 tries to generate the transaction X of amount 10000 cents to be paid by IDPay Code through MIL
     Then the latest transaction creation by merchant through MIL fails because is out of valid period
--- bdd/steps/idpay_code_steps.py
+++ bdd/steps/idpay_code_steps.py
@@ -251,7 +251,8 @@ def step_check_status_instrument_idpay_code(context, status, citizen_name):
                        delay=2, message='Delete card not rejected')
 
 
-@given('the merchant {merchant_name} tries to generates the transaction {trx_name} of amount {amount_cents} cents to be paid by IDPay Code through MIL')
+@given('the merchant {merchant_name} tries to generate the transaction {trx_name} of amount {amount_cents} cents '
+       'to be paid by IDPay Code through MIL')
 def step_when_merchant_try_to_generate_a_transaction_mil(context, merchant_name, trx_name, amount_cents):
     curr_merchant_id = context.merchants[merchant_name]['id']
     curr_merchant_fiscal_code = context.merchants[merchant_name]['fiscal_code']
