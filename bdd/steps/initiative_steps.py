c2043f6912c877fddb9a67f68f2eb3886d0c0750
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2023-11-16 10:20:48 +0100

    fix: [IDP-1992] fix step

--- bdd/steps/initiative_steps.py
+++ bdd/steps/initiative_steps.py
@@ -105,7 +105,7 @@ def step_create_new_initiative(context, initiative_name):
     step_given_initiative_name(context=context, initiative_name=initiative_name)
 
 
-@when('a new whitelist initiative "{initiative_name}"')
+@given('a new whitelist initiative "{initiative_name}"')
 def step_create_new_initiative_with_whitelist(context, initiative_name):
     secrets.initiatives[initiative_name] = {}
     create_initiative_and_update_conf(initiative_name=initiative_name,
